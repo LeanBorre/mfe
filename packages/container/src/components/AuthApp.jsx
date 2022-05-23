776290aae59a914e4e180cce020a9d3cb73bcc20
LeanBorre <leandro.e.borrelli@gmail.com>
2022-05-23 10:22:56 -0300

    Added Auth

---
+++ packages/container/src/components/AuthApp.jsx
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { mount } from 'auth/AuthApp';
+
+export default () => {
+    const ref = useRef(null);
+    const history = useHistory()
+
+    useEffect(() => {
+        const { onParentNavigate } = mount(ref.current, {
+            initialPath: history.location.pathname,
+            onNavigate: ({ pathname: nextPathname }) => {
+                if (history.location.pathname !== nextPathname) {
+                    history.push(nextPathname)
+                }
+            },
+            onSingIn: () => {
+                console.log('user signed in');
+            }
+
+        });
+
+        history.listen(onParentNavigate)
+    }, []);
+
+    return <div ref={ref} />;
+};
